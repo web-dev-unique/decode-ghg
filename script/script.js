c7a772262a776f6ee8d2c0903fbe85bc4e3c39ab
Rajendra patil <133471822+web-dev-unique@users.noreply.github.com>
2024-10-06 13:19:21 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// mobile toggle button
+// mobile toggle button
+// mobile toggle button
+
+const mobile_nav = document.querySelector(".toggle");
+const nav_header = document.querySelector(".header");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
+var swiper = new Swiper(".team", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
